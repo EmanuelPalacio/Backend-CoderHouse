a9f9759a666092b3a405489745acefa64e59380e
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-11-25 16:07:48 -0300

    fifth challenge

--- routes/productos.route.js
+++ routes/productos.route.js
@@ -1,24 +1,52 @@
-/* >> Consigna: Realizar un proyecto de servidor basado en node.js y express que ofrezca una API RESTful de productos. En detalle, que incorpore las siguientes rutas:
-GET '/api/productos' -> devuelve todos los productos.
-GET '/api/productos/:id' -> devuelve un producto según su id.
-POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
-PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
-DELETE '/api/productos/:id' -> elimina un producto según su id.
+/* >> Consigna:  
+1-Utilizando la misma API de productos del proyecto entregable de la clase anterior, construir un web server (no REST) que incorpore:
+
+a-Un formulario de carga de productos en la ruta raíz (configurar la ruta '/productos' para recibir el POST, y redirigir al mismo formulario).
+
+b-Una vista de los productos cargados (utilizando plantillas de handlebars) en la ruta GET '/productos'.
+
+c-Ambas páginas contarán con un botón que redirija a la otra.
  */
 import { Router } from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const routerProductos = Router();
 const products = [
   {
-    title: "nombre del producto",
-    price: "precio",
-    thumbnail: "url al logo o foto del producto",
+    title: "Lenovo Legion",
+    price: 500,
+    thumbnail:
+      "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
     id: 1,
+  },
+  {
+    title: "AMD ryzen 7",
+    price: 300,
+    thumbnail:
+      "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    id: 2,
+  },
+  {
+    title: "mochila",
+    price: 80,
+    thumbnail:
+      "https://media.istockphoto.com/id/1224374453/es/foto/volver-al-concepto-de-la-escuela-mochila-con-material-escolar-sobre-fondo-azul-vista-superior.jpg?b=1&s=170667a&w=0&k=20&c=05lr8WjQg25RLdLFAktZp6j8Q1zVWGe4_2-VGaWkIFY=",
+    id: 3,
+  },
+  {
+    title: "ipad",
+    price: 1000,
+    thumbnail:
+      "https://images.unsplash.com/photo-1542751110-97427bbecf20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1084&q=80",
+    id: 4,
   },
 ];
 
-//obtener array de productgos
+//obtener array de productos
 routerProductos.get("/", (req, res) => {
-  res.json(products);
+  res.render("products", { products });
 });
 
 //agregar un nuevo producto
@@ -44,7 +72,7 @@ routerProductos.get("/:id", (req, res) => {
   verify
     ? ((result = products.find((e) => e.id == Number(id))), (status = 200))
     : ((result = { error: "producto no encontrado" }), (status = 404));
-  res.status(status).json(result);
+  res.status(status).render("product", result);
 });
 
 // actualizar un producto del array
@@ -92,11 +120,14 @@ routerProductos.delete("/:id", (req, res) => {
 });
 
 export default routerProductos;
-/* Para el caso de que un producto no exista, se devolverá el objeto:
-{ error : 'producto no encontrado' }
-Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
-Incorporar el Router de express en la url base '/api/productos' y configurar todas las subrutas en base a este.
-Crear un espacio público de servidor que contenga un documento index.html con un formulario de ingreso de productos con los datos apropiados.
-El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
-Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
+/* Manteniendo la misma funcionalidad reemplazar el motor de plantillas handlebars por pug.
+Manteniendo la misma funcionalidad reemplazar el motor de plantillas handlebars por ejs.
+Por escrito, indicar cuál de los tres motores de plantillas prefieres para tu proyecto y por qué.
+ */
+/* >> Aspectos a incluir en el entregable:
+Realizar las plantillas correspondientes que permitan recorrer el array de productos y representarlo en forma de tabla dinámica, siendo sus cabeceras el nombre de producto, el precio y su foto (la foto se mostrará como un imágen en la tabla)
+En el caso de no encontrarse datos, mostrar el mensaje: 'No hay productos'.
+ */
+/* >> Sugerencias:
+Utilizar iconfinder (https://www.iconfinder.com/free_icons) para obtener la url de las imágenes de los productos (click derecho sobre la imagen -> copiar dirección de la imagen)
  */
