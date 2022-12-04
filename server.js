7267d0f4bfec63d8ee6658810498bfd6df070728
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-12-04 00:09:58 -0300

    socket.oi

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express, { json, urlencoded } from "express";
-import routerProductos from "./routes/productos.route.js";
+import { Server as IOServer } from "socket.io";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 //platilla handlebards
@@ -9,10 +9,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+const products = [];
+const message = [];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/productos", routerProductos);
 app.get("/", (req, res) => {
   res.render("form");
 });
@@ -31,10 +33,36 @@ app.set("view engine", "hbs");
 // se establece donde se encuetran los archivos
 app.set("views", join(__dirname, "public/views"));
 app.use(express.static("public"));
-app.listen(PORT, (error) => {
+
+const expressServer = app.listen(PORT, (error) => {
   if (error) {
     console.log(`erro al escuchar el puerto ${PORT}, error: ${error}`);
   } else {
     console.log(`escuchando puerto ${PORT}`);
   }
+});
+
+const io = new IOServer(expressServer);
+
+io.on("connection", (socket) => {
+  // Logeamos el id del socket que se conecto
+  console.log(`New connection, socket ID: ${socket.id}`);
+  // Cuando se conecta un nuevo cliente le emitimos a ese cliente todos los mensajes que se mandaron hasta el momento
+  socket.emit("server:product", products);
+  // Nos ponesmo a escuchar el evento "client:product" que recibe la info de un mensaje
+  socket.on("product:info", (productInfo) => {
+    // Actualizamos nuestro arreglo de mensajes
+    products.push(productInfo);
+    // Emitimos a TODOS los sockets conectados el arreglo de mensajes actualizado
+    io.emit("server:product", products);
+  });
+  //lo mismo pero para mensajes
+  socket.emit("server:message", message);
+  // Nos ponesmo a escuchar el evento "client:message" que recibe la info de un mensaje
+  socket.on("chat:messageInfo", (messageInfo) => {
+    // Actualizamos nuestro arreglo de mensajes
+    message.push(messageInfo);
+    // Emitimos a TODOS los sockets conectados el arreglo de mensajes actualizado
+    io.emit("server:message", message);
+  });
 });
